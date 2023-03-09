133b9e42dfca756a7abb46b258bd0a45d400286b
PC673 <elshodxushvaqtov07@gmail.com>
2023-03-09 12:53:25 +0500

    Elshod

--- app/src/main/java/uz/itschool/myapplication/Adapter/Food_adapter.kt
+++ app/src/main/java/uz/itschool/myapplication/Adapter/Food_adapter.kt
@@ -1,16 +1,38 @@
 package uz.itschool.myapplication.Adapter
 
+import android.content.ClipData.Item
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.RecycledViewPool
+import uz.itschool.myapplication.Food.Food
 import uz.itschool.myapplication.R
 
-class Food_adapter {
-    class MyHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var item_name=itemView.findViewById<TextView>(R.id.item_name)
-        var img=itemView.findViewById<ImageView>(R.id.item_img)
+class Food_adapter(var menu_items: ArrayList<Food>) :
+    RecyclerView.Adapter<Food_adapter.MyHolder>() {
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var item_name = itemView.findViewById<TextView>(R.id.item_name)
+        var img = itemView.findViewById<ImageView>(R.id.item_img)
+        var cardView = itemView.findViewById<CardView>(R.id.menu_item_bg)
+    }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        return MyHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.menu_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+       val index=menu_items[position]
+        holder.item_name.text=index.name
+        holder.img.setImageResource(index.photo)
+        holder.cardView.setCardBackgroundColor(index.colorBg)
+    }
+
+    override fun getItemCount(): Int {
+return menu_items.size
     }
 }
--- app/src/main/java/uz/itschool/myapplication/MainActivity.kt
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -1,11 +1,72 @@
 package uz.itschool.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import uz.itschool.myapplication.Adapter.Food_adapter
+import uz.itschool.myapplication.Food.Food
+import uz.itschool.myapplication.Food.Ingridient
+import uz.itschool.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val images = ArrayList<Food>()
+        images.add(
+            Food(
+                "Burger",
+                "4.99$",
+                mutableListOf(),
+                R.drawable.burger,
+                Color.parseColor("#556CFF")
+            )
+        )
+        images.add(
+            Food(
+                "Donut",
+                "3.99",
+                mutableListOf(),
+                R.drawable.donut,
+                Color.parseColor("#F5CAC3")
+            )
+        )
+        images.add(
+            Food(
+                "Coffee",
+                "4.11$",
+                mutableListOf(),
+                R.drawable.cofee,
+                Color.parseColor("#B6D7CF")
+            )
+        )
+        images.add(
+            Food(
+                "Fries",
+                "2.99$",
+                mutableListOf(),
+                R.drawable.fries,
+                Color.parseColor("#F28482")
+            )
+        )
+        images.add(
+            Food(
+                "Frenchdog", "1.99", mutableListOf(), R.drawable.frenchdog,
+                Color.parseColor("#ED51B4")
+            )
+        )
+        images.add(
+            Food(
+                "Desert", "0.99$", mutableListOf(), R.drawable.desert,
+                Color.parseColor("#61FA88")
+            )
+        )
+        val adapter = Food_adapter(images)
+        binding.menuRv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        binding.menuRv.adapter = adapter
     }
 }
